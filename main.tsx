711404aa48095671745ee351cab4be3c11c6a067
toolifyw-ship-it <toolifyw@gmail.com>
2026-06-12 01:34:09 +0530

    Update main.tsx

--- main.tsx
+++ main.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './index.ts' // এখানে আমরা সিস্টেমকে বলে দিলাম আসল কোড index.ts ফাইলের ভেতরে আছে
+import App from './App.tsx' 
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
